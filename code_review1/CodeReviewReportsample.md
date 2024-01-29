74e782aafa108d479544271addc52e983a6d8c90
Mai Chí Bảo <mrzaizai2k@gmail.com>
2024-01-29 15:34:59 +0000

    move file to another folder

---
+++ code_review1/CodeReviewReportsample.md
@@ -0,0 +1,3 @@
+The link for code review report is here
+
+https://docs.google.com/spreadsheets/d/1Au_4EWhrUZqFfXZrdVLzhro6ogz5d3XF/edit?usp=drive_link
--- code_review1/DatabaseConnector.java
+++ code_review1/DatabaseConnector.java
@@ -258,7 +258,7 @@ public class DatabaseConnector {
                 }
                 else{
                     return false;
-                }
+                } 
             }
 
 
